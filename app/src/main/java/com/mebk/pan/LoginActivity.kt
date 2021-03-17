7e70bccfbe4cdf0a38778205763f788830e1d9be
yetote <503779938@qq.com>
2021-03-17 22:33:16 +0800

    登录动画

--- app/src/main/java/com/mebk/pan/LoginActivity.kt
+++ app/src/main/java/com/mebk/pan/LoginActivity.kt
@@ -1,14 +1,14 @@
 package com.mebk.pan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import androidx.lifecycle.Observer
 import com.mebk.pan.vm.LoginViewModel
 import kotlinx.android.synthetic.main.activity_login.*
@@ -16,28 +16,39 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<LoginViewModel>()
-
+    private var force: SpringForce? = null
+    private var animation: SpringAnimation? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
 
+        loginActivity_username_et.addTextChangedListener {
+            animation?.cancel()
+            loginActivity_username_textInputLayout.error = null
+        }
+        loginActivity_pwd_et.addTextChangedListener {
+            animation?.cancel()
+            loginActivity_pwd_textInputLayout.error = null
+        }
 
         loginActivity_login_iv.setOnClickListener {
             if (TextUtils.isEmpty(loginActivity_username_et.text.toString())) {
-
+                loginActivity_username_textInputLayout.error = "请输入邮箱"
+                errAnimation(loginActivity_username_textInputLayout)
+            } else if (TextUtils.isEmpty(loginActivity_pwd_et.text.toString())) {
+                loginActivity_pwd_textInputLayout.error = "请输入密码"
+                errAnimation(loginActivity_pwd_textInputLayout)
+            } else {
+                //通过viewModel执行登录
+                viewModel.login(loginActivity_username_et.text.toString(),
+                        loginActivity_pwd_et.text.toString(),
+                        "")
             }
-            if (TextUtils.isEmpty(loginActivity_pwd_et.text.toString())) {
-
-            }
-
-
-            viewModel.login(loginActivity_username_et.text.toString(),
-                    loginActivity_pwd_et.text.toString(),
-                    "")
         }
 
+        //使用vm观察是否登录成功
         viewModel.loginInfo.observe(this, Observer {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
@@ -46,7 +57,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     //登录出错时动画
-    fun errAnimation(){
+    private fun errAnimation(view: View) {
+        force = SpringForce(0f).apply {
+            //设置阻尼，阻尼越大，表示弹簧振动衰减越快
+            dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+            //设置物体刚度，相当于弹簧常量
+            stiffness = SpringForce.STIFFNESS_LOW
+        }
+        //2d旋转动画
+        animation = SpringAnimation(view, SpringAnimation.ROTATION).setSpring(force)
 
+        //设置控件的起始值，这里为起始角度
+        animation!!.setStartValue(10f)
+        animation!!.start()
     }
 }
