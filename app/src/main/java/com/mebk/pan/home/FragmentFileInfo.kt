fa500edd88641a21f4ae2ba9d9cc6f9d048629f0
yetote <503779938@qq.com>
2021-03-30 15:26:30 +0800

    获取下载链接成功

--- app/src/main/java/com/mebk/pan/home/FragmentFileInfo.kt
+++ app/src/main/java/com/mebk/pan/home/FragmentFileInfo.kt
@@ -5,10 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.mebk.pan.R
+import com.mebk.pan.utils.LogUtil
+import com.mebk.pan.vm.FileInfoViewModel
 
 class FragmentFileInfo : Fragment() {
+    private val fileInfoViewModel by viewModels<FileInfoViewModel>()
+
     companion object {
         private const val ARG_ID = "id"
     }
@@ -22,10 +28,19 @@ class FragmentFileInfo : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_file_info, container,false)
+        val view = inflater.inflate(R.layout.fragment_file_info, container, false)
         if (id == null) {
             findNavController().navigate(R.id.action_fragmentFileInfo_to_fragment_directory)
         }
+
+        fileInfoViewModel.download(id!!)
+
+        fileInfoViewModel.downloadClientInfo.observe(viewLifecycleOwner, Observer {
+            LogUtil.err(this.javaClass, it)
+        })
+
+
+
 
 
         return view
