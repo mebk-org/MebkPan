6c35c2ba978b6eb4578752a88e29f99296fd47f5
晓尘 <64715902+fanbo128@users.noreply.github.com>
2021-04-01 20:19:43 +0800

    主题色补充

--- app/src/main/java/com/mebk/pan/MainActivity.java
+++ app/src/main/java/com/mebk/pan/MainActivity.java
@@ -1,49 +1,86 @@
 package com.mebk.pan;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.Toolbar;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity
-{
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
     //几个代表页面的常量
-    Toolbar toolbar;
-    DrawerLayout drawerLayout;
-    ImageView toolbar_imageView;
+    private ViewPager2 v_pager;
+    RadioGroup radioGroup;
     RadioButton radioButton_file;
     RadioButton radioButton_img;
+    List<Fragment> list;
     BottomNavigationView navView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        toolbar_imageView = findViewById(R.id.img_open_life);
-        navView = findViewById(R.id.nav_view);
-        toolbar_imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.open();
-            }
+        navView=findViewById(R.id.nav_view);
 
-            ;
-        });
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+
+
     }
+
+    /**
+     * 创建菜单
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setings, menu); //通过getMenuInflater()方法得到MenuInflater对象，再调用它的inflate()方法就可以给当前活动创建菜单了，第一个参数：用于指定我们通过哪一个资源文件来创建菜单；第二个参数：用于指定我们的菜单项将添加到哪一个Menu对象当中。
+        return true; // true：允许创建的菜单显示出来，false：创建的菜单将无法显示。
+    }
+
+    /**
+     * 菜单的点击事件
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.list_item:
+                Toast.makeText(this, "你点击了 添加！", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * 得到position并进行处理
+     *
+     * @param position
+     */
+    private void msetRb(int position) {
+        switch (position) {
+            case 0:
+                //点亮图标
+                radioButton_file.setChecked(true);
+                break;
+            case 1:
+                radioButton_img.setChecked(true);
+        }
+    }
+
 }
