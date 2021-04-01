e59cf46be3fc70f56631df77e69bd81abae01506
yetote <503779938@qq.com>
2021-04-01 14:41:34 +0800

    优化布局

--- app/src/main/java/com/mebk/pan/vm/MainViewModel.kt
+++ app/src/main/java/com/mebk/pan/vm/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.mebk.pan.vm
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
      val isFileOperator = MutableLiveData<Boolean>().also {
         it.value = false
     }
