5005efdc8353441727e0858a98079ad09110dc85
yetote <503779938@qq.com>
2021-04-10 21:31:55 +0800

    解决后台任务重复调用问题

--- app/src/main/java/com/mebk/pan/home/FragmentHistoryDownload.kt
+++ app/src/main/java/com/mebk/pan/home/FragmentHistoryDownload.kt
@@ -10,12 +10,14 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
 import androidx.work.WorkInfo
 import com.mebk.pan.R
 import com.mebk.pan.aa.DownloadRvAdapter
 import com.mebk.pan.database.entity.DownloadingInfo
 import com.mebk.pan.utils.DOWNLOAD_KEY_PROGRESS
 import com.mebk.pan.utils.LogUtil
+import com.mebk.pan.utils.RetrofitClient
 import com.mebk.pan.vm.HistoryDownloadViewModel
 import com.mebk.pan.vm.MainViewModel
 import kotlinx.android.synthetic.main.rv_item_history_download_waiting.view.*
@@ -31,26 +33,12 @@ class FragmentHistoryDownload : Fragment() {
 
         rv = view.findViewById(R.id.fragment_history_download_rv)
         rv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        (rv.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
         adapter = DownloadRvAdapter(listview, requireActivity())
         rv.adapter = adapter
-
         mainViewModel.downloadListInfo.observe(viewLifecycleOwner, {
 
         })
-
-        mainViewModel.downloadWorkInfo.observe(viewLifecycleOwner, { workInfo ->
-            workInfo.forEach {
-                if (it.state == WorkInfo.State.RUNNING) {
-                    val progress = it.progress.getInt(DOWNLOAD_KEY_PROGRESS, 0)
-                    LogUtil.err(this.javaClass, "progress=$progress")
-                    val viewHolder = rv.findViewHolderForAdapterPosition(0)
-                    viewHolder?.let { vh ->
-                        vh.itemView.rv_item_history_download_waiting_progress.progress = progress
-                    }
-                }
-            }
-        })
-
 
         downloadVewModel.downloadInfo.observe(viewLifecycleOwner, {
             listview.clear()
@@ -59,9 +47,48 @@ class FragmentHistoryDownload : Fragment() {
 
         })
 
-        mainViewModel.downloadProgressWorkInfo.observe(viewLifecycleOwner, Observer {
+        mainViewModel.downloadWorkInfo.observe(viewLifecycleOwner, {
+
+            when (it[mainViewModel.currentPos].state) {
+                WorkInfo.State.ENQUEUED -> {
+                    if (listview.isNotEmpty()) {
+                        listview[mainViewModel.currentPos].state = RetrofitClient.DOWNLOAD_STATE_DOWNLOADING
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+                WorkInfo.State.RUNNING -> {
+                    if (listview.isNotEmpty()) {
+                        if (listview[mainViewModel.currentPos].state != RetrofitClient.DOWNLOAD_STATE_DOWNLOADING) {
+                            listview[mainViewModel.currentPos].state = RetrofitClient.DOWNLOAD_STATE_DOWNLOADING
+                            adapter.notifyDataSetChanged()
+                        }
+                        val progress = it[0].progress.getInt(DOWNLOAD_KEY_PROGRESS, 0)
+                        LogUtil.err(this.javaClass, "progress=$progress")
+                        val viewHolder = rv.findViewHolderForAdapterPosition(mainViewModel.currentPos)
+                        viewHolder?.let { vh ->
+                            vh.itemView.rv_item_history_download_waiting_progress.progress = progress
+                        }
+                    }
+                }
+                WorkInfo.State.SUCCEEDED -> {
+//                    if (listview.isNotEmpty()) {
+//                        listview.removeFirst()
+//                        adapter.notifyDataSetChanged()
+//                    }
+                }
+                WorkInfo.State.CANCELLED -> {
+
+                }
+                WorkInfo.State.FAILED -> {
+
+                }
+            }
 
         })
+
+
+
+
 
 
         return view
