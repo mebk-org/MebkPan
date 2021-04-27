637f4bece956a1516b7d1570c85c27fb05d9c4c8
yetote <503779938@qq.com>
2021-04-27 15:51:58 +0800

    输入分享密码完成

--- app/src/main/java/com/mebk/pan/MainActivity.java
+++ app/src/main/java/com/mebk/pan/MainActivity.java
@@ -5,23 +5,25 @@ import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.constraintlayout.widget.Group;
@@ -34,15 +36,12 @@ import androidx.transition.TransitionManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputLayout;
 import com.mebk.pan.database.entity.File;
-import com.mebk.pan.utils.LogUtil;
 import com.mebk.pan.utils.ToolUtilsKt;
 import com.mebk.pan.vm.MainViewModel;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static androidx.navigation.fragment.NavHostFragment.findNavController;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -59,7 +58,15 @@ public class MainActivity extends AppCompatActivity {
     private int fabWidth;
     private int fabRadius;
     private NavHostFragment navHostFragment;
+    private View sharePopupWindowLayout, pwdPopupWindowLayout;
+    private PopupWindow sharePopupwindow, pwdPopupwindow;
+    private TextView sharePwdTv;
+    private SwitchCompat sharePwdSwitch;
+    private Button sharePwdSureBtn, getSharePwdCancelBtn;
+    private TextInputLayout sharePwdTextTextInputLayout;
+    private EditText sharePwdEditText;
     public static final int MOVE_CODE = 1;
+    private String sharePwd;
     private final ActivityResultLauncher<Intent> moveResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
         public void onActivityResult(ActivityResult result) {
@@ -114,16 +121,12 @@ public class MainActivity extends AppCompatActivity {
             if (mainViewModel.getCheckList().size() != 1) {
                 Toast.makeText(this, "只能分享单个文件", Toast.LENGTH_SHORT).show();
             } else {
+                sharePopupwindow.showAtLocation(rootLayout, Gravity.BOTTOM, 0, 0);
 
-                View sharePopupWindowLayout = LayoutInflater.from(this).inflate(R.layout.popupwindow_share, null);
-                PopupWindow popupWindow = new PopupWindow(sharePopupWindowLayout, ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT);
-                popupWindow.showAtLocation(rootLayout, Gravity.BOTTOM, 0, 0);
-
-
-                File file = mainViewModel.getCheckList().get(0);
-                String id = file.getId();
-                boolean isDir = !file.getType().equals("file");
-                mainViewModel.shareFile(id, isDir, "", -1, 86400, true, 0);
+//                File file = mainViewModel.getCheckList().get(0);
+//                String id = file.getId();
+//                boolean isDir = !file.getType().equals("file");
+//                mainViewModel.shareFile(id, isDir, "", -1, 86400, true, 0);
             }
         });
 
@@ -160,36 +163,44 @@ public class MainActivity extends AppCompatActivity {
             animatorSet.start();
         });
 
+        sharePwdSwitch.setOnClickListener(v -> {
+            sharePwd();
+        });
+
+        sharePwdTv.setOnClickListener(v -> {
+            sharePwd();
+        });
+
+        sharePwdSureBtn.setOnClickListener(v -> {
+            if (TextUtils.isEmpty(sharePwdEditText.getText().toString())) {
+                sharePwdTextTextInputLayout.setError("请设置分享密码");
+            } else {
+                sharePwdTextTextInputLayout.setError(null);
+                sharePwd = sharePwdEditText.getText().toString();
+                pwdPopupwindow.dismiss();
+                Log.e(TAG, "onClick: sharePwd=" + sharePwd);
+            }
+        });
+
     }
 
-    /**
-     * 创建菜单
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.setings, menu); //通过getMenuInflater()方法得到MenuInflater对象，再调用它的inflate()方法就可以给当前活动创建菜单了，第一个参数：用于指定我们通过哪一个资源文件来创建菜单；第二个参数：用于指定我们的菜单项将添加到哪一个Menu对象当中。
-        return true; // true：允许创建的菜单显示出来，false：创建的菜单将无法显示。
+    private void sharePwd() {
+        pwdPopupwindow.setFocusable(true);
+        pwdPopupwindow.update();
+        pwdPopupwindow.showAtLocation(rootLayout, Gravity.BOTTOM, 0, 0);
     }
 
-    /**
-     * 菜单的点击事件
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.list_item:
-                Toast.makeText(this, "你点击了 添加！", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                break;
-        }
-
-        return true;
-    }
 
     private void initView() {
         bottomNavigationView = findViewById(R.id.nav_bottom_view);
         rootLayout = findViewById(R.id.container);
+
+        sharePopupWindowLayout = LayoutInflater.from(this).inflate(R.layout.popupwindow_share, null);
+        pwdPopupWindowLayout = LayoutInflater.from(this).inflate(R.layout.popupwindow_share_pwd, null);
+
+        sharePwdTv = sharePopupWindowLayout.findViewById(R.id.popupwindow_share_pwd_tv);
+        sharePwdSwitch = sharePopupWindowLayout.findViewById(R.id.popupwindow_share_pwd_switch);
+
         TabLayout tabLayout = findViewById(R.id.tabLayout);
 
         downloadItem = tabLayout.getTabAt(0);
@@ -205,23 +216,17 @@ public class MainActivity extends AppCompatActivity {
         uploadGroup = findViewById(R.id.gp_upload);
         shareGroup = findViewById(R.id.gp_share);
         mkdirGroup = findViewById(R.id.gp_mkdir);
+
+        sharePopupwindow = new PopupWindow(sharePopupWindowLayout, ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT);
+        pwdPopupwindow = new PopupWindow(pwdPopupWindowLayout, ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT);
+
+        sharePwdSureBtn = pwdPopupWindowLayout.findViewById(R.id.popupwindow_share_pwd_sure_btn);
+        getSharePwdCancelBtn = pwdPopupWindowLayout.findViewById(R.id.popupwindow_share_pwd_cancel_btn);
+
+        sharePwdTextTextInputLayout = pwdPopupWindowLayout.findViewById(R.id.popupwindow_share_pwd_textInputLayout);
+        sharePwdEditText = pwdPopupWindowLayout.findViewById(R.id.popupwindow_share_pwd_et);
     }
 
-    /**
-     * 得到position并进行处理
-     *
-     * @param position
-     */
-    private void msetRb(int position) {
-        switch (position) {
-            case 0:
-                //点亮图标
-                radioButton_file.setChecked(true);
-                break;
-            case 1:
-                radioButton_img.setChecked(true);
-        }
-    }
 
     /**
      * 文件选择动画
