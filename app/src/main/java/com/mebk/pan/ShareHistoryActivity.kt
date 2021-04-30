01865dc9daddce60a7c4acf3df4e7ee61c8ec8bc
yetote <503779938@qq.com>
2021-04-30 22:21:12 +0800

    分享页面正在加载完成

--- app/src/main/java/com/mebk/pan/ShareHistoryActivity.kt
+++ app/src/main/java/com/mebk/pan/ShareHistoryActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.mebk.pan.aa.ShareHistoryRvAdapter
 import com.mebk.pan.application.MyApplication
 import com.mebk.pan.database.entity.ShareHistoryEntity
@@ -14,6 +15,7 @@ import com.mebk.pan.vm.ShareHistoryViewModel
 
 class ShareHistoryActivity : AppCompatActivity() {
     private lateinit var rv: RecyclerView
+    private lateinit var sr: SwipeRefreshLayout
     private val list = mutableListOf<ShareHistoryEntity>()
     private lateinit var adapter: ShareHistoryRvAdapter
 
@@ -24,8 +26,10 @@ class ShareHistoryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_share_history)
 
         viewModel.getShareHistory(MyApplication.uid!!, 1)
-
+        sr = findViewById(R.id.shareHistory_sr)
         rv = findViewById(R.id.shareHistory_rv)
+
+        sr.isRefreshing = true
         adapter = ShareHistoryRvAdapter(this, list)
         adapter.setClickListener {
             val bundle = Bundle().apply {
@@ -41,10 +45,12 @@ class ShareHistoryActivity : AppCompatActivity() {
         rv.adapter = adapter
 
         viewModel.shareHistoryInfo.observe(this, {
+            sr.isRefreshing = false
             list.clear()
             list.addAll(it)
             LogUtil.err(this.javaClass, "list=$list")
             adapter.notifyDataSetChanged()
+
         })
     }
 }
